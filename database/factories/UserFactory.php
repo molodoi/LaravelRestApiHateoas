9ff42c53c7de28192558a05dd9a667f3143180a1
Matthieu Lamerant <matthieu.lamerant@goandlive.tech>
2018-01-17 16:26:09 +0100

    Factories and seeding database 10-049

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,11 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\User;
+use App\Models\Product;
+use App\Models\Category;
+use App\Models\Transaction;
+use App\Models\Seller;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +18,7 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(User::class, function (Faker $faker) {
     static $password;
 
     return [
@@ -21,5 +26,43 @@ $factory->define(App\User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+        'verified' => $verified = $faker->randomElement([User::VERIFIED_USER, User::UNVERIFIED_USER]),
+        'verification_token' => $verified == User::VERIFIED_USER? null : User::generateVerificationCode(),
+        'admin' => $verified = $faker->randomElement([User::ADMIN_USER, User::REGULAR_USER]),
+
+    ];
+});
+
+
+$factory->define(Category::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+    ];
+});
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->numberBetween(1, 10),
+        'status' => $faker->randomElement([Product::AVAILABLE_PRODUCT, Product::UNAVAILABLE_PRODUCT]),
+        'image' => $faker->randomElement(['1.jpg','2.jpg','3.jpg']),
+        'seller_id' => User::all()->random()->id,
+        // same as : User::inRandomOrder()->first()->id
+    ];
+});
+
+$factory->define(Transaction::class, function (Faker $faker) {
+
+	// Get random seller have products 
+	$seller = Seller::has('products')->get()->random();
+	// Get random user without seller
+	$buyer = User::all()->except($seller->id)->random();
+
+    return [
+        'quantity' => $faker->numberBetween(1, 3),
+        'buyer_id' => $buyer->id,
+        'product_id' => $seller->products->random()->id,
     ];
 });
--- readme.md
+++ readme.md
@@ -1,2 +1,16 @@
 LaravelRestApiHateoas
 ========================
+Build a RESTfull API Hateoas with Laravel 5
+
+Content
+========================
+Learn how to:
+
+
+Install
+========================
+1 - Clone : git clone https://github.com/molodoi/LaravelRestApiHateoas.git
+2 - Make : composer install
+3 - Create database name restfullapi
+4 - Make : php artisan migrate 
+5 - Make : php artisan db:seed
