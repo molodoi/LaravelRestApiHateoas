e9855628d3589154a24abc2f2a0bbf8a84c6c613
Matthieu Lamerant <matthieu.lamerant@goandlive.tech>
2018-01-25 10:33:52 +0100

    Implementing CRUD operations Category - 18 081

--- app/Http/Controllers/Category/CategoryController.php
+++ app/Http/Controllers/Category/CategoryController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers\Category;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
 use App\Http\Controllers\ApiController;
 
 class CategoryController extends ApiController
@@ -14,17 +17,9 @@ class CategoryController extends ApiController
      */
     public function index()
     {
-        //
-    }
+        $categories = Category::all();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return $this->showAll($categories);
     }
 
     /**
@@ -35,51 +30,64 @@ class CategoryController extends ApiController
      */
     public function store(Request $request)
     {
-        //
+        $rules = [
+            'name' =>'required',
+            'description' => 'required',
+        ]; 
+
+        $this->validate($request, $rules);
+
+        $newcategory = Category::create($request->all());
+
+        return $this->showOne($newcategory, 201);
+
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Models\Category  $category
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Category $category)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        return $this->showOne($category);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Models\Category  $category
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Category $category)
     {
-        //
+        $category->fill($request->intersect([
+            'name',
+            'description',   
+        ]));
+
+        if($category->isClean()){
+            return $this->errorResponse('You need to specify any different value to update', 422);
+        }
+
+        $category->save();
+
+        return $this->showOne($category);
+
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Models\Category  $category
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Category $category)
     {
-        //
+        $category->delete();
+
+        return $this->showOne($category);
     }
 }
