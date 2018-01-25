e1b18998ae4c2ba1fe346e8e3f0f28d6c62fbd33
Matthieu Lamerant <matthieu.lamerant@goandlive.tech>
2018-01-25 11:07:36 +0100

    Implementing complex operations for Transaction RelashionShip Categories/Seller - 21 087

--- app/Http/Controllers/Transaction/TransactionController.php
+++ app/Http/Controllers/Transaction/TransactionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Transaction;
 
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 use App\Http\Controllers\ApiController;
 
@@ -14,29 +15,11 @@ class TransactionController extends ApiController
      */
     public function index()
     {
-        //
+        $transactions = Transaction::all();
+
+        return $this->showAll($transactions);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
@@ -44,42 +27,9 @@ class TransactionController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Transaction $transaction)
     {
-        //
+        return $this->showOne($transaction);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
---
+++ app/Http/Controllers/Transaction/TransactionSellerController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Transaction;
+
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class TransactionSellerController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Transaction $transaction)
+    {
+        // Récupérer le vendeur d'un produit sur une transaction
+        $seller = $transaction->product->seller;
+
+        return $this->showOne($seller);
+    }
+}
