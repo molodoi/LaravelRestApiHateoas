00883e460dbc38b09c46af54c370a450541e24d0
Matthieu Lamerant <matthieu.lamerant@goandlive.tech>
2018-01-29 17:46:49 +0100

    27 120

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,9 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use App\Models\Product;
+use App\User;
+use App\Mail\UserCreated;
+use Illuminate\Support\Facades\Mail;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,13 @@ class AppServiceProvider extends ServiceProvider
         // https://laravel-news.com/laravel-5-4-key-too-long-error
         Schema::defaultStringLength(191);
 
+        // Send verification mail using event
+        User::created(function($user) {
+            retry(5, function() use ($user) {
+                Mail::to($user)->send(new UserCreated($user));
+            }, 100);
+        });
+
         // Event update : Vérifier/changer le statut du produit à chaque changement
         Product::updated(function($product) {
             if ($product->quantity == 0 && $product->isAvailable()) {
@@ -26,6 +36,8 @@ class AppServiceProvider extends ServiceProvider
                 $product->save();
             }
         });
+
+
     }
 
     /**
